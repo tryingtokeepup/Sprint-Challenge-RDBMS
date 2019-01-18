4d0b0d51cd38899005e33fae0d358b35b9629498
tryingtokeepup <kl.hawaii@gmail.com>
2019-01-18 09:08:33 -0800

    All migration tables built, with relationships. Will seed for my own benifit.

--- data/migrations/20190118085659_actions.js
+++ data/migrations/20190118085659_actions.js
@@ -1,8 +1,27 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("actions", function(tbl) {
+    // need a primary key, as always
+    // many to one relationship, so I WILL need a foreign key
+    tbl.increments();
+    // need a name column, unique
+    tbl
+      .string("name", 255)
+      .notNullable()
+      .unique();
+    // generic discription column, telling us what must be done
+    tbl.text("description").notNullable();
+    // a foreign key that links to the projects table, linking to the primary key, the id column
+    tbl
+      .integer("project_id")
+      .unsigned()
+      .references("id")
+      .inTable("projects");
+    // a flag to tell us whether or not something is complete!
+
+    tbl.boolean("complete").notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("actions");
 };
